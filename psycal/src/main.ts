43a5a1e032bd7d786c8c67787ead73b8a82daf82
PaulIanCapanas <97999879+PaulIanCapanas@users.noreply.github.com>
2023-05-16 09:07:36 +0800

    Update main.ts
    Change Class Name

--- psycal/src/main.ts
+++ psycal/src/main.ts
@@ -12,52 +12,52 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
          <input class = "form" type="text" id="fname" size="42"  placeholder="0">
       </tr>
      <tr>
-        <td><button class = "button" id="sin(" type="button">sin</button> </td>
-        <td><button class = "button" id="cos(" type="button">cos</button> </td>
-        <td><button class = "button" id="tan(" type="button">tan</button></td>
+        <td><button class = "button1" id="sin(" type="button">sin</button> </td>
+        <td><button class = "button1" id="cos(" type="button">cos</button> </td>
+        <td><button class = "button1" id="tan(" type="button">tan</button></td>
      </tr>
      <tr>
-      <td><button class = "button" id="arcsin(" type="button">arcSin</button></td>
-      <td><button class = "button" id="arccos(" type="button">arcCos</button></td>
-      <td><button class = "button" id="arctan(" type="button">arcTan</button></td>
+      <td><button class = "button1" id="arcsin(" type="button">arcSin</button></td>
+      <td><button class = "button1" id="arccos(" type="button">arcCos</button></td>
+      <td><button class = "button1" id="arctan(" type="button">arcTan</button></td>
      </tr>
      <tr>
-      <td><button class = "button" id="log(" type="button">log</button></td>
-      <td><button class = "button" id="ln(" type="button">ln</button></td>
-      <td><button class = "button" id="√(" type="button">√</button></td>
+      <td><button class = "button1" id="log(" type="button">log</button></td>
+      <td><button class = "button1" id="ln(" type="button">ln</button></td>
+      <td><button class = "button1" id="√(" type="button">√</button></td>
      </tr>
    </table>
 <!-- lower -->
   <table>
      <tr>
-      <td><button class = "button" id="^" type="button">^</button></td>
-      <td><button class = "button" id="(" type="button">(</button></td>
-      <td><button class = "button" id=")" type="button">)</button></td>
-      <td><button class = "button" id="clear" type="button">AC</button></td>
+      <td><button class = "button3" id="^" type="button">^</button></td>
+      <td><button class = "button3" id="(" type="button">(</button></td>
+      <td><button class = "button3" id=")" type="button">)</button></td>
+      <td><button class = "button3" id="clear" type="button">AC</button></td>
      </tr>
      <tr>
-        <td><button class = "button" id="7" type="button">7</button></td>
-        <td><button class = "button" id="8" type="button">8</button></td>
-        <td><button class = "button" id="9" type="button">9</button></td>
-        <td><button class = "button" id="/" type="button">÷</button></td>
+        <td><button class = "button2" id="7" type="button">7</button></td>
+        <td><button class = "button2" id="8" type="button">8</button></td>
+        <td><button class = "button2" id="9" type="button">9</button></td>
+        <td><button class = "button3" id="/" type="button">÷</button></td>
      </tr>
      <tr>
-        <td><button class = "button" id="4" type="button">4</button></td>
-        <td><button class = "button" id="5" type="button">5</button></td>
-        <td><button class = "button" id="6" type="button">6</button></td>
-        <td><button class = "button" id="*" type="button">x</button></td>
+        <td><button class = "button2" id="4" type="button">4</button></td>
+        <td><button class = "button2" id="5" type="button">5</button></td>
+        <td><button class = "button2" id="6" type="button">6</button></td>
+        <td><button class = "button3" id="*" type="button">x</button></td>
      </tr>
      <tr>
-        <td><button class = "button" id="1" type="button">1</button></td>
-        <td><button class = "button" id="2" type="button">2</button></td>
-        <td><button class = "button" id="3" type="button">3</button></td>
-        <td><button class = "button" id="-" type="button">-</button></td>
+        <td><button class = "button2" id="1" type="button">1</button></td>
+        <td><button class = "button2" id="2" type="button">2</button></td>
+        <td><button class = "button2" id="3" type="button">3</button></td>
+        <td><button class = "button3" id="-" type="button">-</button></td>
      </tr>
      <tr>
-        <td><button class = "button" id="0" type="button">0</button></td>
-        <td><button class = "button" id="." type="button">.</button></td>
-        <td><button class = "button" id="=" type="button">=</button></td>
-        <td><button class = "button" id="+" type="button">+</button></td>
+        <td><button class = "button2" id="0" type="button">0</button></td>
+        <td><button class = "button2" id="." type="button">.</button></td>
+        <td><button class = "button2" id="=" type="button">=</button></td>
+        <td><button class = "button3" id="+" type="button">+</button></td>
      </tr>
     </table>
    </div> 
@@ -67,7 +67,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 let output = document.getElementById('fname') as HTMLInputElement
 let solver = new ScientificCalculator
-let buttons = document.querySelectorAll('.button')
+let buttons = document.querySelectorAll('.button2, .button1, .button3')
 
 if (buttons) {
   buttons.forEach(button => {
