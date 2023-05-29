4a733c75077291250b8558177d44c92a867b9009
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2023-05-29 21:43:06 +0800

    added some technical stuff

--- psycal/src/components/Stack.ts
+++ psycal/src/components/Stack.ts
@@ -1,26 +1,37 @@
 export default class Stack<T> {
-    private data: T[] = Object.seal(new Array(16).fill(undefined))
-    private count = 0
-  
-    push(element: T) {
-      this.data[this.count] = element
-      this.count++
-    }
-  
-    pop(): T {
-      this.count--
-      return this.data[this.count]
-    }
-  
-    peek(): T {
-      return this.data[this.count - 1]
-    }
-  
-    isEmpty(): boolean {
-      return this.count <= 0
-    }
-  
-    isFull(): boolean {
-      return this.count >= this.data.length
-    }
+  private data: T[] = Object.seal(new Array(16).fill(undefined))
+  private count = 0
+
+  push(element: T) {
+    this.data[this.count] = element
+    this.count++
   }
+
+  pop(): T {
+    this.count--
+    return this.data[this.count]
+  }
+
+  peek(): T {
+    return this.data[this.count - 1]
+  }
+
+  isEmpty(): boolean {
+    return this.count <= 0
+  }
+
+  isFull(): boolean {
+    return this.count >= this.data.length
+  }
+
+  displayContents() {
+    const contents = this.data.slice(0, this.count);
+    return contents.join(' ')
+  }
+}
+
+let testStack = new Stack
+testStack.push(7)
+testStack.push(3)
+testStack.push(5)
+console.log(testStack.displayContents())
